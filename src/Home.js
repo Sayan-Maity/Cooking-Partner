51b92f518416ecdb0ea7bb8ffc23219e1e762a4e
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-29 04:50:56 +0530

    home.js file is added

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
       <main className="font-fancy w-3/5  mx-auto justify-center block  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  rounded-md ">
         <div className="justify-center items-center bg-slate-100/80 rounded-md p-24">
           <h1 className="text-5xl mx-auto mb-7 text-center text-rose-500">
-            Meal Idea Generation App
+            Your Cooking Partner
           </h1>
           <ul className="w-auto text-center text-stone-700 text-2xl">
             <li className="my-3 cursor-pointer hover:text-stone-500">
