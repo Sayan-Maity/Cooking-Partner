67bdb420a7f54ae75c3950b4d5c68b2a251ee592
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-12-29 04:25:07 +0530

    package.json files tailwind.config and .gitignore files are added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#bada55",
+      },
+      fontFamily: {
+        fancy: ["Dancing Script"],
+      },
+    },
+  },
+  plugins: [],
+};
